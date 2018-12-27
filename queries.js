65502f35d787c8c392551fd70258a77da374d781
Manikandan Jayapalan <mj@MJs-MacBook-Air.local>
2018-12-27 14:22:01 -0500

    Post method modification

--- queries.js
+++ queries.js
@@ -32,11 +32,11 @@ const getUserById = (request, response) => {
 const createUser = (request, response) => {
   const { name, email } = request.body
 
-  pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
+  pool.query('INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id', [name, email], (error, result) =>{
     if (error) {
       throw error
     }
-    response.status(201).send(`User added with ID: ${results.ID}`)
+    response.status(201).json(result.rows[0].id)
   })
 }
 
